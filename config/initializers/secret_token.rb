34411a0546b635dab8464ff93e3ba223e06f627c
romeo <sarutc@gmail.com>
2011-04-24 22:12:55 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpexch::Application.config.secret_token = 'cbbebf76cc7fbb7fb9921aa3d27ac550a025153b123a0fafaec0ab55ef2acc0065160423d48daa9d859d8ca9c8429506a8ee98cf386f0e3cb1007b6e2865a792'
